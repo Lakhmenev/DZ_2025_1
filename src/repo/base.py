7f2021aa8cf254af0e5e40b708a184686d7ec62d
Lakhmenev <99120629+Lakhmenev@users.noreply.github.com>
2025-04-02 23:19:02 +0300

    Правка добавления отелей, добавляем схему BaseModel для понимания с какой сущностью мы работаем по схеме. Так как класс базовый то реализуем это через pydantic BaseModel!!!

--- src/repo/base.py
+++ src/repo/base.py
@@ -1,4 +1,5 @@
 from sqlalchemy import select, insert
+from pydantic import BaseModel
 
 
 class BaseRepository:
@@ -17,8 +18,7 @@ class BaseRepository:
         result = await self.session.execute(query)
         return result.scalars().one_or_none()
 
-    async def add(self, data):
-        print('1')
+    async def add(self, data: BaseModel):
         add_data_stmt = insert(self.model).values(**data.model_dump()).returning(self.model)
         result = await self.session.execute(add_data_stmt)
         return result.scalars().one()
