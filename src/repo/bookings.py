87a70edf701a4557e60a0fb43a23bde508892535
Lakhmenev <99120629+Lakhmenev@users.noreply.github.com>
2025-07-22 20:59:08 +0300

    add repo add_booking

--- src/repo/bookings.py
+++ src/repo/bookings.py
@@ -21,3 +21,16 @@ class BookingsRepository(BaseRepository):
         )
         res = await self.session.execute(query)
         return [self.mapper.map_to_domain_entity(booking) for booking in res.scalars().all()]
+
+    async def add_booking(self, data: BookingAddDTO, hotel_id: int):
+        rooms_ids_to_get = rooms_ids_for_booking(
+            date_from=data.date_from,
+            date_to=data.date_to,
+            hotel_id=hotel_id,
+        )
+        rooms_ids_to_book_res = await self.session.execute(rooms_ids_to_get)
+        rooms_ids_to_book: Sequence[int] = rooms_ids_to_book_res.scalars().all()
+
+        if data.room_id in rooms_ids_to_book:
+            new_booking = await self.add(data)
+            return new_booking
